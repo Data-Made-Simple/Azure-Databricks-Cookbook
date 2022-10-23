1c261c7ce580955bb1970c97ff67a5dcb5f3559e
Data-Made-Simple <75577036+Data-Made-Simple@users.noreply.github.com>
2022-10-23 14:03:20 -0400

    Create AzureCloudShell-Kevin.ps1

---
+++ Chapter01/AzureCloudShell-Kevin.ps1
@@ -0,0 +1,13 @@
+﻿$appId="89bbd353-90df-4153-b1e5-05636710af77"
+$appSecret="89bbd353-90df-4153-b1e5-05636710af77"
+$tenantId="828623f3-62b1-4edd-aef5-a3563727cea5"
+$subscriptionName="23Oct-Subscription"
+$resourceGroup = "CookbookRG2"
+
+az login --service-principal --username $appId --password $appSecret --tenant $TenantId
+
+az account set --subscription $subscriptionName
+
+az group create --name $resourceGroup --location "East US"
+
+az databricks workspace create --resource-group $resourceGroup --name BigDataWorkspace --location "East US" --sku standard
